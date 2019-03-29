405da008ba4e355b57f9241cf60a28eea50ba394
lizongxing <1026078111@qq.com>
2019-03-29 21:50:48 +0800

    fix: 修复dockerfile
    修复dockerfile

--- hooks/build.sh
+++ hooks/build.sh
@@ -3,4 +3,4 @@ USERNAME=lzx1026078111
 PASSWORD=H8C-te5-GeJ-eQh
 echo $USERNAME
 echo $PASSWORD
-docker build -t $IMAGE_NAME --no-cache --build-arg USERNAME=$USERNAME --build-arg PASSWORD=$PASSWORD.
+docker build -t $IMAGE_NAME --no-cache --build-arg USERNAME=$USERNAME --build-arg PASSWORD=$PASSWORD .

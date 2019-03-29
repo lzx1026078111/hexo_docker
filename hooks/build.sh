71eb4e8e84358208bf326808b7a5b609d4f2e30b
lizongxing <1026078111@qq.com>
2019-03-29 21:24:39 +0800

    fix: 修复dockerfile
    修复dockerfile

--- hooks/build.sh
+++ hooks/build.sh
@@ -1,2 +1,4 @@
 #!/usr/bin bash
-docker build -t $IMAGE_NAME . --build-arg username=$username --build-arg password=$password
+echo $USERNAME
+echo $PASSWORD
+docker build -t $IMAGE_NAME . --build-arg USERNAME=$USERNAME --build-arg PASSWORD=$PASSWORD
